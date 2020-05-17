4d42b5d32beeac707b2bd6ddc86fa3eae7ac12f3
thecassion <pierrerobentz.cassion@gmail.com>
2020-05-17 18:50:18 -0400

    Remove obsolete function

--- boto3_helper.py
+++ boto3_helper.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from io import StringIO
 from trp import Document, Cell, Table
 import json
-
+from datetime import date
+import re
 
 def startJob(s3BucketName, objectName):
     response = None
@@ -58,83 +59,10 @@ def getJobResults(jobId):
         nextToken = None
         if('NextToken' in response):
             nextToken = response['NextToken']
-    # print(pages)
-    # f = open("test_{}.json".format(jobId), "w")
-    # f.write(json.loads(pages))
-    # f.close()
     return pages
 
-def generate_table_csv(table_result, blocks_map, table_index):
-    #rows = Table(table_result,blocks_map).rows
-    rows = get_rows_columns_map(table_result, blocks_map)
-
-    csv = ""
-    for row_index, cols in rows.items():
-
-        for col_index, text in cols.items():
-            csv += '{}'.format(text) + ","
-        csv += '\n'
-
-    csv += '\n\n\n'
-    return csv
-
-def get_rows_columns_map(table_result, blocks_map):
-    rows = {}
-    if('Relationships' in table_result and table_result['Relationships']):
-        for relationship in table_result['Relationships']:
-            if relationship['Type'] == 'CHILD':
-                for child_id in relationship['Ids']:
-                    cell = blocks_map[child_id]
-                    if cell['BlockType'] == 'CELL':
-                        row_index = cell['RowIndex']
-                        col_index = cell['ColumnIndex']
-                        if row_index not in rows:
-                            # create new row
-                            rows[row_index] = {}
-
-                        # get the text value
-                        rows[row_index][col_index] = get_text(cell, blocks_map)
-    return rows
 
 
-def get_text(result, blocks_map):
-    text = ''
-    if 'Relationships' in result:
-        for relationship in result['Relationships']:
-            if relationship['Type'] == 'CHILD':
-                for child_id in relationship['Ids']:
-                    word = blocks_map[child_id]
-                    if word['BlockType'] == 'WORD':
-                        text += word['Text'] + ' '
-                    if word['BlockType'] == 'SELECTION_ELEMENT':
-                        if word['SelectionStatus'] =='SELECTED':
-                            text +=  'X '
-    return text
-
-
-def get_table_responses(response):
-    response_item = response
-
-    # Get the text blocks
-    blocks=response_item['Blocks']
-    #pprint.pprint(blocks)
-
-    blocks_map = {}
-    table_blocks = []
-    for block in blocks:
-        blocks_map[block['Id']] = block
-        if block['BlockType'] == "TABLE":
-            table_blocks.append(block)
-
-    if len(table_blocks) <= 0:
-        print("<b> NO Table FOUND </b>")
-
-    csvs = []
-    for index, table in enumerate(table_blocks):
-        csvs.append(generate_table_csv(table, blocks_map, index +1))
-        #csv += '\n\n'
-
-    return csvs
 def generate_csv_from_table(table):
     csv = ""
     for r, row in enumerate(table.rows):
@@ -162,8 +90,6 @@ def get_tables_from_pdf(s3BucketName,documentName):
     elif(status=="FAILED"):
         return {"message":status}
 
-from datetime import date
-import re
 
 def find_column(col,regex):
     if re.match(regex,col) and (re.match(regex,col)!=None):
